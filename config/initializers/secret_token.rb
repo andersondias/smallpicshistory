2a54b35e1abdec70916a6ca3122dedc646bdffbf
Anderson dias <andersondaraujo@gmail.com>
2010-12-30 14:51:32 -0300

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smallpicshistory::Application.config.secret_token = 'a959ed23d12177bfbf91c2119d8480d4db87d9139286ff4015f804fc39ed0175d32cf1ec6ad46de7e08d993b3ffc78d9da3468581c761e4114d5c0335d4e4e79'
